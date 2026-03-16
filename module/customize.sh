55af9ad6a3a36c96d61c8c5123445a73bd8bca04
StevenWin818 <awen0818@hotmail.com>
2026-03-16 22:10:44 +0800

    添加 META-INF 文件，修复代码 Bug

--- module/customize.sh
+++ module/customize.sh
@@ -7,11 +7,13 @@ ui_print "- 正在挂载系统目录..."
 
 # 设置文件与目录权限 (非常关键)
 set_perm_recursive "$MODPATH/system/product/app/MITSMClient" 0 0 0755 0644
+set_perm_recursive "$MODPATH/system/product/app/UPTsmService" 0 0 0755 0644
+set_perm_recursive "$MODPATH/system/product/app/MINextpay" 0 0 0755 0644
 
 ui_print "- 正在清理可能导致 UID 冲突的残留数据..."
 rm -rf /data/data/com.miui.tsmclient
 rm -rf /data/user_de/0/com.miui.tsmclient
 
-ui_print "- 请手动关闭对NFC (android.uid.nfc) 的"卸载模块"功能。"
+ui_print "- 请手动关闭对NFC (android.uid.nfc) 的\"卸载模块\"功能。"
 
 ui_print "- 安装完成！请重启设备以应用更改。"
